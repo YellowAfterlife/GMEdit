e3f2f78a71bb735d65f426d84182677e19d26b2e
YellowAfterlife <yellowafterlife@ymail.com>
2018-05-21 14:24:47 +0300

    event_inherited|action_inherited are now handled before project wide completion to have AI work as GMS2 compatibility script.

--- src/ui/KeyboardShortcuts.hx
+++ src/ui/KeyboardShortcuts.hx
@@ -299,8 +299,6 @@ class KeyboardShortcuts {
 			return true;
 		}
 		//
-		if (openLocal(term, pos, null)) return true;
-		//
 		if (term == "event_inherited" || term == "action_inherited") {
 			var def = gml.GmlScopes.get(pos.row);
 			if (def == "") return false;
@@ -313,6 +311,8 @@ class KeyboardShortcuts {
 			}
 			return true;
 		}
+		//
+		if (openLocal(term, pos, null)) return true;
 		//
 		var helpURL = GmlAPI.helpURL;
 		if (helpURL != null) {
