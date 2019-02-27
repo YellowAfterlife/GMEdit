af6d0c3363de8bc67311ee7a97448ea2a0fbddb2
YellowAfterlife <yellowafterlife@ymail.com>
2019-02-27 23:09:19 +0200

    we'll get to keybinds some other day

---
+++ src/editors/EditKeybinds.hx
@@ -0,0 +1,13 @@
+package editors;
+
+/**
+ * ...
+ * @author YellowAfterlife
+ */
+class EditKeybinds {
+	/*
+	aceEditor.commands.bindKey to rebind
+	aceEditor.commands.commandKeyBinding for current
+	aceEditor.commands.commands for available commands
+	*/
+}
