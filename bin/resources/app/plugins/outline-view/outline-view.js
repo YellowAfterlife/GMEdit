d2d18bf643ccab12bbd87febb6bb25faf6b967f7
YellowAfterlife <yellowafterlife@ymail.com>
2019-03-09 11:05:01 +0200

    Reorganize outline-view to support nesting and multiple modes

--- bin/resources/app/plugins/outline-view/outline-view.js
+++ bin/resources/app/plugins/outline-view/outline-view.js
@@ -1,13 +1,73 @@
 (function() {
+	function setc(el, name, on) {
+		if (el.classList.contains(name) != !!on) {
+			el.classList.toggle(name);
+		}
+	}
+	function seta(el, attr, val) {
+		if (val != null) {
+			el.setAttribute(attr, val);
+		} else el.removeAttribute(attr);
+	}
 	//
 	var popout = false; // show a popout instead of a sidebar
 	var currOnly = false; // original idea (only show the current file)
-	var lightDef = false; // poll current definition from Ace status bar
+	var showAtTop = true;
 	//
-	// items (definitions)
-	var rxDef = /^(?:#event|#define|#moment|#section|#roomcc)\b\s*(\w+(?::\w+)?)(.*)$/;
-	// subitems (regions/landmarks)
-	var rxMark = /^\s*((?:#region|\/\/#region|\/\/#mark)\b\s*(.*))$/;
+	var escapeProp = $gmedit["tools.NativeString"].escapeProp;
+	var modeMap = {
+		"ace/mode/gml": (function() {
+			var rxDef = /^(?:(#event)|#define|#moment|#section|#roomcc)\b\s*(\w+(?::\w+)?)(.*)$/;
+			var rxPush = /^\s*((?:#region|\/\/#region)\b\s*(.*))$/;
+			var rxPop = /^\s*(?:#endregion|\/\/#endregion)\b/;
+			var rxMark = /^\s*(\/\/#mark\b\s*(.*))$/;
+			var rxCtx = /^\s*((?:#region|\/\/#region|\/\/#mark)\b.*)$/
+			function update(file, pos) {
+				var def = pos.def, row = pos.row;
+				var ctx = null, ctxRow = null;
+				var session = file.codeEditor.session;
+				for (; row >= 0; row--) {
+					var rowText = session.getLine(row), mt;
+					if (mt = rxDef.exec(rowText)) { def = mt[2]; break; }
+					if (!ctx && (mt = rxCtx.exec(rowText))) {
+						ctx = mt[1];
+						ctxRow = row;
+						if (def != null) break;
+					}
+				}
+				pos.def = def;
+				pos.ctx = ctx;
+				pos.row = ctxRow != null ? ctxRow : row;
+			}
+			function reindex(file, ctx) {
+				var doc = file.codeEditor.session.doc;
+				var n = doc.getLength();
+				var def = null, mt, nav;
+				for (var i = 0; i < n; i++) {
+					var line = doc.getLine(i);
+					if (mt = rxDef.exec(line)) {
+						def = mt[2];
+						var txt = def;
+						if (def != "properties") {
+							var tail = mt[3].trim();
+							if (tail) txt += " ➜ " + tail; // narrow space, arrow, narrow space
+						}
+						nav = { def: def,ctxAfter:true,showAtTop:showAtTop };
+						if (mt[1]) nav.outlineViewData = "gml_" + def;
+						ctx.flush(txt, txt, nav);
+						continue;
+					}
+					else if (mt = rxPush.exec(line)) ctx.push(mt[2], mt[2], {def:def,ctx:mt[1],ctxAfter:true,showAtTop:showAtTop});
+					else if (mt = rxPop.exec(line)) ctx.pop();
+					else if (mt = rxMark.exec(line)) ctx.mark(mt[2], mt[2], {def:def,ctx:mt[1],ctxAfter:true,showAtTop:showAtTop});
+				}
+			}
+			return {
+				update: update,
+				reindex: reindex
+			}
+		})(),
+	};
 	//
 	var outer = document.createElement("div");
 	outer.id = "outline-view";
@@ -22,11 +82,9 @@
 	//
 	var currEl = null;
 	//
-	function currFile() {
-		return $gmedit["gml.file.GmlFile"].current;
-	}
+	function currFile() { return $gmedit["gml.file.GmlFile"].current; }
 	// maybe this should be a "built-in" function instead
-	function activate(file) {
+	function activateFile(file) {
 		var tabs = document.querySelectorAll(".chrome-tab");
 		for (var i = 0; i < tabs.length; i++) {
 			if (tabs[i].gmlFile == file) {
@@ -37,143 +95,158 @@
 		return null;
 	}
 	//
-	var escapeProp = $gmedit["tools.NativeString"].escapeProp;
-	function updateCurrentDefSave(file) {
-		var ctr = file.outlineView;
-		if (!ctr) return;
-		var curr = treeview.querySelector(".outline-current-file .outline-current-def");
-		if (curr) curr.classList.remove("outline-current-def");
-		//
-		var query = null, itemq = null;
-		var session = file.codeEditor.session;
-		for (var row = session.selection.lead.row; row >= 0; row--) {
-			var rowText = session.getLine(row);
-			var mt = rxDef.exec(rowText);
-			if (mt != null) {
-				query = '.item[outline-def="'+escapeProp(mt[1])+'"]';
-				break;
-			}
-			if (!itemq) {
-				mt = rxMark.exec(rowText);
-				if (mt) itemq = '[title="'+escapeProp(mt[1])+'"]';
-			}
-		}
-		//
-		if (itemq) query += itemq;
-		var item = ctr.querySelector(query);
-		if (item) item.classList.add("outline-current-def");
-	}
-	function updateCurrentDef(file, ctr, def) {
-		var curr = treeview.querySelector(".outline-current-file .outline-current-def");
-		if (curr) curr.classList.remove("outline-current-def");
-		if (!def) return;
-		//
-		var query = '.item[outline-def="'+escapeProp(def)+'"]';
-		var item = null;
-		if (ctr.querySelector(query+'.ctx')) {
-			var session = file.codeEditor.session;
-			for (var row = session.selection.lead.row; row >= 0; row--) {
-				var rowText = session.getLine(row);
-				var mt = rxDef.exec(rowText);
-				if (mt != null) break;
-				mt = rxMark.exec(rowText);
-				if (mt) {
-					item = ctr.querySelector(query+'[title="'+escapeProp(mt[1])+'"]');
-					if (item) break;
-				}
-			}
-		}
-		//
-		if (!item) item = ctr.querySelector(query);
-		if (item) item.classList.add("outline-current-def");
-	}
-	//
-	var makeItem = $gmedit["ui.treeview.TreeView"].makeItem;
+	var TreeView = $gmedit["ui.treeview.TreeView"];
+	var makeDir = TreeView.makeDir;
 	var navPool = [];
-	function makeNav(file, label, title, evt, nav) {
+	function makeNav_clicked(e) {
+		var dir = e.target;
+		if (dir.classList.contains("header")) dir = dir.parentElement;
+		if (e.offsetX < dir.treeHeader.querySelector("span").offsetLeft && dir.treeItems.children.length > 0) {
+			dir.classList.toggle("open");
+		} else {
+			if (currFile() != dir.outlineViewFile) activateFile(dir.outlineViewFile);
+			if (dir.outlineViewNav) dir.outlineViewFile.navigate(dir.outlineViewNav);
+		}
+	}
+	function makeNav(file, label, title, nav) {
 		var r = navPool.pop();
 		if (r) {
-			r.querySelector("span").textContent = label;
-		} else r = makeItem(label);
-		r.setAttribute("outline-event", evt||"");
-		r.setAttribute("outline-def", nav.def);
-		r.title = title;
-		// items navigating to inside of events get a "ctx" class for indication
-		if (!!nav.ctx != r.classList.contains("ctx")) {
-			r.classList.toggle("ctx");
+			r.querySelector(".header span").textContent = label;
+		} else {
+			r = makeDir(label);
+			r.classList.add("outline-item");
+			r.classList.remove("dir");
+			r.treeHeader.classList.add("item");
+			r.treeHeader.addEventListener("click", makeNav_clicked);
 		}
-		r.onclick = function(_) {
-			function finish() {
-				file.navigate(nav);
-				updateCurrentDef(file, file.outlineView, nav.def);
-				aceEditor.scrollToLine(aceEditor.selection.lead.row);
-				aceEditor.focus();
+		r.classList.add("open");
+		r.outlineViewFile = file;
+		r.outlineViewNav = nav;
+		// if it's a top-level node, we let it be affected by custom CSS icon
+		seta(r.treeHeader, "data-full-path", nav ? null : file.path);
+		seta(r.treeHeader, "data-ident", nav ? null : file.name);
+		if (!nav && file.path != null) {
+			var q = TreeView.element.querySelector('.item[data-full-path="'+escapeProp(file.path)+'"]');
+			if (q) {
+				var th = q.getAttribute("data-thumb");
+				if (th != null) r.treeHeader.setAttribute("data-thumb", th);
 			}
-			if (currFile() != file) {
-				if (activate(file)) {
-					finish();
-					//setTimeout(finish);
-				}
-			} else finish();
-			return false;
-		};
+		}
+		seta(r, "outline-data", nav && nav.outlineViewData)
+		seta(r, "title", title);
+		seta(r, "outline-def", nav && nav.def);
+		seta(r, "outline-ctx", nav && nav.ctx);
 		return r;
+	}
+	//
+	function update(file) {
+		var session = file.codeEditor.session;
+		var conf = modeMap[session.$modeId];
+		if (!conf) return;
+		var pos = {
+			row: session.selection.lead.row,
+			def: null,
+			ctx: null,
+		}
+		conf.update(file, pos);
+		//
+		var currDir = treeview.querySelector(".outline-current-file");
+		var currItem = currDir.querySelector(".outline-current-item");
+		// if cursor is after a mark/subregion, try to highlight that,
+		// or, if it's not on the sidebar yet, the nearest subregion (upwards from there)
+		var nextItem;
+		if (pos.ctx) {
+			var prefix = pos.def ? '.outline-item[outline-def="'+escapeProp(pos.def)+'"] ' : '';
+			nextItem = currDir.querySelector(prefix+'.outline-item[outline-ctx="'+escapeProp(pos.ctx)+'"]');
+			while (!nextItem) {
+				pos.row -= 1;
+				conf.update(file, pos);
+				if (pos.ctx == null) break;
+				nextItem = currDir.querySelector(prefix+'.outline-item[outline-ctx="'+escapeProp(pos.ctx)+'"]');
+			}
+		} else if (pos.def != null) {
+			nextItem = currDir.querySelector('.outline-item[outline-def="'+escapeProp(pos.def)+'"]');
+		} else nextItem = null;
+		//
+		if (currItem != nextItem) {
+			if (currItem) currItem.classList.remove("outline-current-item");
+			if (nextItem) nextItem.classList.add("outline-current-item");
+		}
 	}
 	function reindex(file) {
 		var ov = file.outlineView;
-		var el = ov.treeItems;
 		if (!file.codeEditor) return;
-		// pool up the existing items:
-		var cs = el.children;
-		for (var i = 0; i < cs.length; i++) navPool.push(cs[i]);
-		el.innerHTML = "";
-		// now we add items for events/regions
-		var doc = file.codeEditor.session.doc;
-		var n = doc.getLength();
-		var def = null;
-		for (var i = 0; i < n; i++) {
-			var mt = rxDef.exec(doc.getLine(i));
-			if (mt) {
-				def = mt[1];
-				var txt = def;
-				if (def != "properties") {
-					var tail = mt[2].trim();
-					if (tail) txt += " ➜ " + tail; // narrow space, arrow, narrow space
-				}
-				var evt = mt[0].startsWith("#e") ? def : null;
-				el.appendChild(makeNav(file, txt, txt, evt, {
-					def: def,
-					showAtTop: true
-				}));
-				continue;
-			}
-			else if (mt = rxMark.exec(doc.getLine(i))) {
-				el.appendChild(makeNav(file, mt[2], mt[0], null, {
-					def: def,
-					ctx: mt[1],
-					ctxAfter: true,
-					showAtTop: true
-				}));
-			}
+		// memorize which 
+		var closed = ov.treeItems.querySelectorAll(".outline-item:not(.open)");
+		var reclose = [];
+		for (var i = 0; i < closed.length; i++) {
+			var q = closed[i];
+			var def = q.getAttribute("outline-def");
+			var ctx = q.getAttribute("outline-ctx");
+			var query = '.outline-item[outline-def="'+escapeProp()+'"]'
+			if (ctx) query += ' .outline-item[outline-ctx="'+escapeProp(ctx) + '"]';
+			reclose.push(query);
 		}
-		// in what is no less than a bit of a hack, for items with no children
-		// we shall simply untag .dir from container, and tag the header with .item
-		var th = ov.treeHeader;
-		if ((el.children.length == 0) != th.classList.contains("item")) {
-			ov.classList.toggle("dir");
-			th.classList.toggle("item");
+		// pool up the existing items (should we bother, really?):
+		var old = ov.treeItems.querySelectorAll(".outline-item");
+		for (var i = 0; i < old.length; i++) {
+			var q = old[i];
+			q.outlineViewFile = null;
+			q.outlineViewNav = null;
+			q.treeHeader.removeAttribute("outline-data");
+			q.treeHeader.removeAttribute("data-thumb");
+			q.treeItems.innerHTML = "";
+			navPool.push(q);
+		}
+		ov.treeItems.innerHTML = "";
+		//
+		var conf = modeMap[file.codeEditor.session.$modeId];
+		if (!conf) return;
+		var stack = [];
+		var curr = ov;
+		function finishDir(q) {
+			setc(q, "outline-dir", q.treeItems.children.length > 0);
+		}
+		function flushStack() {
+			while (stack.length > 0) finishDir(stack.pop());
+			finishDir(curr);
+		}
+		var ctx = {
+			flush: function(label, title, nav) {
+				flushStack();
+				var q = makeNav(file, label, title, nav);
+				ov.treeItems.appendChild(q);
+				return (curr = q);
+			},
+			push: function(label, title, nav) {
+				var q = makeNav(file, label, title, nav);
+				curr.treeItems.appendChild(q);
+				stack.push(curr);
+				return (curr = q);
+			},
+			pop: function() {
+				finishDir(curr);
+				return (curr = stack.pop());
+			},
+			mark: function(label, title, nav) {
+				var q = makeNav(file, label, title, nav);
+				finishDir(q);
+				curr.treeItems.appendChild(q);
+				return q;
+			},
+		};
+		conf.reindex(file, ctx);
+		flushStack();
+		finishDir(ov);
+		// re-collapse:
+		for (var i = 0; i < reclose.length; i++) {
+			var q = ov.treeItems.querySelector(reclose[i]);
+			if (q) q.classList.remove("open");
 		}
 	}
 	//
 	function createFor(file) {
-		var dir = $gmedit["ui.treeview.TreeView"].makeDir(file.name);
-		dir.classList.add("open");
-		dir.treeHeader.addEventListener("click", function(_) {
-			// mirroring above, clicking a childless item should open it instead
-			if (dir.classList.contains("dir")) {
-				dir.classList.toggle("open");
-			} else activate(file);
-		});
+		var dir = makeNav(file, file.name, file.path, null);
 		file.outlineView = dir;
 		reindex(file);
 	}
@@ -223,7 +296,7 @@
 	}
 	function onFileSave(e) {
 		reindex(e.file);
-		updateCurrentDefSave(e.file);
+		update(e.file);
 	}
 	function onTabsReorder(e) {
 		syncAll(e.target.tabEls);
@@ -232,10 +305,7 @@
 	var onUpdate_scheduled = false;
 	function onUpdate() {
 		onUpdate_scheduled = false;
-		var ctxEl = document.querySelector(".ace_status-hint .context");
-		var ctx = (ctxEl && ctxEl.style.display != "none") ? ctxEl.innerText : null;
-		var ctr = treeview.querySelector('.outline-current-file');
-		updateCurrentDef(currFile(), ctr, ctx);
+		update(currFile());
 	}
 	function onUpdate_schedule() {
 		if (onUpdate_scheduled) return;
@@ -243,54 +313,45 @@
 		setTimeout(onUpdate, 120);
 	}
 	//
-	var hidden = true;
+	var visible = false;
 	function toggle() {
-		hidden = !hidden;
-		if (!hidden) {
-			if (popout) {
-				document.body.insertBefore(outer, document.querySelector("#preferences-window"));
-			} else {
+		visible = !visible;
+		function sete(obj, event, listener, on) {
+			if (on) {
+				obj.on(event, listener);
+			} else obj.off(event, listener);
+		}
+		sete(GMEdit, "activeFileChange", onFileChange, visible);
+		sete(GMEdit, "fileClose", onFileClose, visible);
+		sete(GMEdit, "fileSave", onFileSave, visible);
+		sete(GMEdit, "tabsReorder", onTabsReorder, visible);
+		sete(aceEditor, "changeStatus", onUpdate_schedule, visible);
+		sete(aceEditor, "changeSelection", onUpdate_schedule, visible);
+		sete(aceEditor, "keyboardActivity", onUpdate_schedule, visible);
+		if (visible) {
+			if (!popout) {
 				GMEdit.sidebar.add("Outline View", outer);
 				GMEdit.sidebar.set("Outline View");
-			}
-			GMEdit.on("activeFileChange", onFileChange);
-			GMEdit.on("fileClose", onFileClose);
-			GMEdit.on("fileSave", onFileSave);
-			GMEdit.on("tabsReorder", onTabsReorder);
-			aceEditor.on("changeStatus", onUpdate_schedule);
-			aceEditor.on("changeSelection", onUpdate_schedule);
-			aceEditor.on("keyboardActivity", onUpdate_schedule);
-		} else {
-			if (popout) {
-				outer.parentElement.removeChild(outer);
-			} else {
-				GMEdit.sidebar.remove("Outline View", outer);
-			}
-			GMEdit.off("activeFileChange", onFileChange);
-			GMEdit.off("fileClose", onFileClose);
-			GMEdit.off("fileSave", onFileSave);
-			GMEdit.off("tabsReorder", onTabsReorder);
-			aceEditor.off("changeStatus", onUpdate_schedule);
-			aceEditor.off("changeSelection", onUpdate_schedule);
-			aceEditor.off("keyboardActivity", onUpdate_schedule);
-		}
-		if (!hidden) {
-			if (currOnly) {
-				changeTo(currFile());
-			} else {
+			} else document.body.insertBefore(outer, document.querySelector("#preferences-window"));
+			//
+			if (!currOnly) {
 				syncAll();
 				changeTo_post(currFile());
 				onUpdate_schedule();
-			}
+			} else changeTo(currFile());
+		} else {
+			if (!popout) {
+				GMEdit.sidebar.remove("Outline View", outer);
+			} else outer.parentElement.removeChild(outer);
 		}
 	}
 	function init() {
 		AceCommands.add({
 			name: "toggleOutlineView",
-			//bindKey: "Ctrl-Shift-O",
+			//bindKey: "Ctrl-Shift-O", // if you'd like
 			exec: function(editor) {
 				toggle();
-				localStorage.setItem("outline-view-hide", hidden);
+				localStorage.setItem("outline-view-hide", !visible);
 			}
 		});
 		AceCommands.addToPalette({
@@ -304,6 +365,7 @@
 		init: init,
 		cleanup: function() {
 			// todo
-		}
+		},
+		_modeMap: modeMap
 	});
 })();
