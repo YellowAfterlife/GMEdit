df1a3aa1ae8b9df205ff40c18a39ae014309fc03
YellowAfterlife <yellowafterlife@hotmail.com>
2019-07-25 10:40:25 +0300

    temp

---
+++ src/parsers/GmlLinter.hx
@@ -0,0 +1,13 @@
+package parsers;
+import tools.Aliases;
+
+/**
+ * ...
+ * @author YellowAfterlife
+ */
+class GmlLinter {
+	public static function run(source:GmlCode):Bool {
+		var q = new GmlReaderExt(source);
+	}
+	//public static function runFor(editor
+}
