50d4b757ea633c2f74c27fc5547dd0a49808c17b
YellowAfterlife <yellowafterlife@hotmail.com>
2019-11-28 01:09:26 +0200

    Allow navigating to top-level function <name> definitions.

--- src/file/kind/KGml.hx
+++ src/file/kind/KGml.hx
@@ -90,7 +90,7 @@ class KGml extends KCode {
 		var row = 0, col = 0;
 		var i:Int, s:String;
 		if (nav.def != null) {
-			var rxDef = new RegExp("^(#define|#event|#moment|#target)[ \t]" + NativeString.escapeRx(nav.def) + "\\b");
+			var rxDef = new RegExp("^(#define|#event|#moment|#target|function)[ \t]" + NativeString.escapeRx(nav.def) + "\\b");
 			i = 0;
 			while (i < len) {
 				s = session.getLine(i);
