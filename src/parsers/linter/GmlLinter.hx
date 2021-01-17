6595194c9f2c3a6dbd9a64576ac0797551ddadd7
YellowAfterlife <yellowafterlife@hotmail.com>
2021-01-18 01:35:47 +0200

    Fixed a linter crash when a function looks like it should have type parameters but doesn't.

--- src/parsers/linter/GmlLinter.hx
+++ src/parsers/linter/GmlLinter.hx
@@ -265,7 +265,7 @@ class GmlLinter {
 		var templateTypes:Array<GmlType> = null;
 		if (doc != null) {
 			argTypes = doc.argTypes;
-			argTypeLast = doc.rest ? argTypes.length - 1 : 0x7fffffff;
+			argTypeLast = doc.rest && argTypes != null ? argTypes.length - 1 : 0x7fffffff;
 			if (doc.templateNames != null) {
 				templateTypes = NativeArray.create(doc.templateNames.length);
 			}
