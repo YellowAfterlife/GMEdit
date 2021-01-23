e14f144f592651eca6d054324d96001767a34b77
Liam Nobel <liamnobel@yahoo.com>
2021-01-23 16:35:19 -0500

    Added Shader Creation Support for GMS 1.4

--- src/gmx/GmxManip.hx
+++ src/gmx/GmxManip.hx
@@ -84,6 +84,7 @@ class GmxManip {
 		} else {
 			var ntxt:String = switch (single) {
 				case "script": plural + "\\" + name + ".gml";
+				case "shader": plural + "\\" + name + ".shader";
 				default: plural + "\\" + name;
 			};
 			switch (single) {
@@ -127,6 +128,43 @@ class GmxManip {
 					GmlSeeker.runSync(pj.fullPath(q.npath), tpl, q.name, KGmxEvents.inst);
 					GmlFile.open(q.name, pj.fullPath(q.npath));
 				};
+				case "shader": {
+					q.npath = '$plural/$name.shader';
+					var tmp = '//
+// Simple passthrough vertex shader
+//
+attribute vec3 in_Position;                  // (x,y,z)
+//attribute vec3 in_Normal;                  // (x,y,z)     unused in this shader.	
+attribute vec4 in_Colour;                    // (r,g,b,a)
+attribute vec2 in_TextureCoord;              // (u,v)
+
+varying vec2 v_vTexcoord;
+varying vec4 v_vColour;
+
+void main()
+{
+    vec4 object_space_pos = vec4( in_Position.x, in_Position.y, in_Position.z, 1.0);
+    gl_Position = gm_Matrices[MATRIX_WORLD_VIEW_PROJECTION] * object_space_pos;
+    
+    v_vColour = in_Colour;
+    v_vTexcoord = in_TextureCoord;
+}
+
+//######################_==_YOYO_SHADER_MARKER_==_######################@~//
+// Simple passthrough fragment shader
+//
+varying vec2 v_vTexcoord;
+varying vec4 v_vColour;
+
+void main()
+{
+    gl_FragColor = v_vColour * texture2D( gm_BaseTexture, v_vTexcoord );
+}
+
+';
+					pj.writeTextFileSync(q.npath, tmp);
+					GmlFile.open(q.name, pj.fullPath(q.npath));
+				}
 			}
 			ngmx = new SfGmx(single, ntxt);
 		}
