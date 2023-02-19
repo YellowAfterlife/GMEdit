1baae8f16666a59461d17a9727eb9d03dbb0815e
YellowAfterlife <yellowafterlife@hotmail.com>
2023-02-20 00:55:00 +0200

    Global search now forms a queue and works better with projects that contain incredible amounts (read: thousands) of searchable resources.

--- src/yy/YySearcher.hx
+++ src/yy/YySearcher.hx
@@ -15,16 +15,42 @@ import yy.YyTimeline;
  * @author YellowAfterlife
  */
 class YySearcher {
+	var queue:Array<{ fn:Any->Void, arg:Any }> = [];
+	var count = 0;
+	static inline var maxCount = 16;
+	var done:Void->Void;
+	function new(done:Void->Void = null) {
+		this.done = done;
+	}
+	function procSync<T>(arg:T, fn:T->Void):Void {
+		count++;
+		fn(arg);
+	}
+	function procNext() {
+		count--;
+		var pair = queue.shift();
+		if (pair != null) {
+			procSync(pair.arg, pair.fn);
+		} else if (count <= 0) done();
+	}
+	function proc<T>(arg:T, fn:T->Void):Void {
+		if (count < maxCount) {
+			procSync(arg, fn);
+		} else queue.push({ fn: cast fn, arg: arg });
+	}
+	
 	public static function run(
-		pj:Project, fn:ProjectSearcher, done:Void->Void, opt:GlobalSearchOpt
+		_project:Project, fn:ProjectSearcher, done:Void->Void, opt:GlobalSearchOpt
 	):Void {
-		var yyProject:YyProject = pj.readYyFileSync(pj.name);
-		var scriptLambdas = pj.properties.lambdaMode == Scripts;
-		var rxName = Project.rxName;
-		var filesLeft = 1;
+		var ctx = new YySearcher(done);
 		inline function next():Void {
-			if (--filesLeft <= 0) done();
+			ctx.procNext();
 		}
+		
+		var yyProject:YyProject = _project.readYyFileSync(_project.name);
+		var scriptLambdas = _project.properties.lambdaMode == Scripts;
+		var rxName = Project.rxName;
+		
 		function addError(s:String) {
 			if (opt.errors != null) {
 				opt.errors += "\n" + s;
@@ -32,151 +58,173 @@ class YySearcher {
 		}
 		var v22 = yyProject.resourceType != "GMProject";
 		for (resPair in yyProject.resources) {
-			var resName:String, resPath:RelPath, resFull:String, resType:String;
+			var _resName:String, _resPath:RelPath, _resFull:String, _resType:String;
 			if (v22) {
 				var resVal = resPair.Value;
-				resPath = resVal.resourcePath;
-				resType = resVal.resourceType;
-				resName = null;
+				_resPath = resVal.resourcePath;
+				_resType = resVal.resourceType;
+				_resName = null;
 			} else {
-				resPath = resPair.id.path;
-				resType = pj.yyResourceTypes[resPair.id.name];
+				_resPath = resPair.id.path;
+				_resType = _project.yyResourceTypes[resPair.id.name];
 			}
-			resFull = pj.fullPath(resPath);
+			_resFull = _project.fullPath(_resPath);
 			inline function ensureName():Void {
 				if (v22) {
-					resName = rxName.replace(resPath, "$1");
-				} else resName = resPair.id.name;
+					_resName = rxName.replace(_resPath, "$1");
+				} else _resName = resPair.id.name;
 			}
-			switch (resType) {
+			inline function packResCtx() {
+				return {
+					project: _project,
+					resName: _resName,
+					resPath: _resPath,
+					resFull: _resFull,
+				};
+			}
+			switch (_resType) {
 				case "GMScript": if (opt.checkScripts) {
 					ensureName();
 					if (!scriptLambdas
 						|| !opt.expandLambdas
-						|| !NativeString.startsWith(resName, GmlExtLambda.lfPrefix)
-					) {
-						filesLeft += 1;
-						var gmlPath = Path.withExtension(resPath, "gml");
-						pj.readTextFile(gmlPath, function(error, code) {
+						|| !NativeString.startsWith(_resName, GmlExtLambda.lfPrefix)
+					) ctx.proc(packResCtx(), function(resCtx) {
+						var gmlPath = Path.withExtension(resCtx.resPath, "gml");
+						var gmlFull = Path.withExtension(resCtx.resFull, "gml");
+						resCtx.project.readTextFile(gmlPath, function(error, code) {
 							if (error == null) {
-								var gml1 = fn(resName, gmlPath, code);
+								var gml1 = fn(resCtx.resName, gmlFull, code);
 								if (gml1 != null && gml1 != code) {
-									pj.writeTextFileSync(gmlPath, gml1);
+									resCtx.project.writeTextFileSync(gmlPath, gml1);
 								}
 							} else Main.console.warn(error);
 							next();
 						});
-					}
+					});
 				};
 				case "GMObject": if (opt.checkObjects) {
 					ensureName();
-					filesLeft += 1;
-					pj.readYyFile(resPath, function(error, obj:YyObject) {
-						if (error == null) try {
-							var code = obj.getCode(resPath);
-							var gml1 = fn(resName, resFull, code);
-							if (gml1 != null && gml1 != code) {
-								if (obj.setCode(resPath, gml1)) {
-									pj.writeYyFileSync(resPath, obj);
-								} else addError("Failed to modify " + resName
-									+ ":\n" + YyObject.errorText);
-							}
-						} catch (x:Dynamic) {
-							addError("Failed to modify " + resName + ":\n" + x);
-						} else Main.console.warn(error);
-						next();
+					ctx.proc(packResCtx(), function(resCtx) {
+						var resPath = resCtx.resPath;
+						resCtx.project.readYyFile(resPath, function(error, obj:YyObject) {
+							if (error == null) try {
+								var code = obj.getCode(resPath);
+								var gml1 = fn(resCtx.resName, resCtx.resFull, code);
+								if (gml1 != null && gml1 != code) {
+									if (obj.setCode(resPath, gml1)) {
+										resCtx.project.writeYyFileSync(resPath, obj);
+									} else addError("Failed to modify " + resCtx.resName
+										+ ":\n" + YyObject.errorText);
+								}
+							} catch (x:Dynamic) {
+								addError("Failed to modify " + resCtx.resName + ":\n" + x);
+							} else Main.console.warn(error);
+							next();
+						});
 					});
 				};
-				case "GMTimeline": if (opt.checkObjects) {
+				case "GMTimeline": if (opt.checkTimelines) {
 					ensureName();
-					filesLeft += 1;
-					pj.readYyFile(resPath, function(error, _tl:YyTimelineImpl) {
-						var tl:YyTimeline = _tl;
-						if (error == null) try {
-							var code = tl.getCode(resFull);
-							var gml1 = fn(resName, resFull, code);
-							if (gml1 != null && gml1 != code) {
-								if (tl.setCode(resPath, gml1)) {
-									pj.writeYyFileSync(resPath, tl);
-								} else addError("Failed to modify " + resName
-									+ ":\n" + YyObject.errorText);
-							}
-						} catch (x:Dynamic) {
-							addError("Failed to modify " + resName + ":\n" + x);
-						} else Main.console.warn(error);
-						next();
+					ctx.proc(packResCtx(), function(resCtx) {
+						resCtx.project.readYyFile(resCtx.resPath, function(error, _tl:YyTimelineImpl) {
+							var tl:YyTimeline = _tl;
+							if (error == null) try {
+								var code = tl.getCode(resCtx.resFull);
+								var gml1 = fn(resCtx.resName, resCtx.resFull, code);
+								if (gml1 != null && gml1 != code) {
+									if (tl.setCode(resCtx.resPath, gml1)) {
+										resCtx.project.writeYyFileSync(resCtx.resPath, tl);
+									} else addError("Failed to modify " + resCtx.resName
+										+ ":\n" + YyObject.errorText);
+								}
+							} catch (x:Dynamic) {
+								addError("Failed to modify " + resCtx.resName + ":\n" + x);
+							} else Main.console.warn(error);
+							next();
+						});
 					});
 				};
 				case "GMShader": if (opt.checkShaders) {
 					ensureName();
-					var shPath:String;
 					inline function procShader(ext:String, type:String) {
-						shPath = Path.withExtension(resPath, ext);
-						pj.readTextFile(shPath, function(err, code) {
-							if (err == null) {
-								var gml1 = fn(resName + '($type)', shPath, code);
-								if (gml1 != null && gml1 != code) {
-									pj.writeTextFileSync(shPath, gml1);
-								}
-							} else Main.console.warn(err);
-							next();
+						ctx.proc({
+							project: _project,
+							resName: _resName + '($type)',
+							shPath: Path.withExtension(_resPath, ext),
+							shFull: Path.withExtension(_resFull, ext),
+						}, function(shCtx) {
+							shCtx.project.readTextFile(shCtx.shPath, function(err, code) {
+								if (err == null) {
+									var gml1 = fn(shCtx.resName, shCtx.shFull, code);
+									if (gml1 != null && gml1 != code) {
+										shCtx.project.writeTextFileSync(shCtx.shPath, gml1);
+									}
+								} else Main.console.warn(err);
+								next();
+							});
 						});
 					}
-					filesLeft += 2;
 					procShader("fsh", "fragment");
 					procShader("vsh", "vertex");
 				};
 				case "GMExtension": if (opt.checkExtensions) {
 					ensureName();
-					if (opt.expandLambdas && resName == GmlExtLambda.extensionName) continue;
-					filesLeft += 1;
-					pj.readYyFile(resPath, function(err, ext:YyExtension) {
+					if (opt.expandLambdas && _resName == GmlExtLambda.extensionName) continue;
+					ctx.proc(packResCtx(), function(resCtx) resCtx.project.readYyFile(resCtx.resPath,
+					function(err, ext:YyExtension) {
 						if (err != null) {
 							Main.console.warn(err);
 							next();
 							return;
 						}
-						var extDir = Path.directory(resPath);
-						for (file in ext.files) {
-							var fileName = file.filename;
-							if (Path.extension(fileName).toLowerCase() != "gml") continue;
-							var filePath = Path.join([extDir, fileName]);
-							filesLeft += 1;
-							pj.readTextFile(filePath, function(err, code) {
+						var extDir = Path.directory(resCtx.resPath);
+						var extDirFull = Path.directory(resCtx.resFull);
+						for (file in ext.files) if (
+							tools.PathTools.ptExt(file.filename) == "gml"
+						) ctx.proc({
+							project: resCtx.project,
+							fileName: file.filename,
+							filePath: Path.join([extDir, file.filename]),
+							fileFull: Path.join([extDirFull, file.filename]),
+						}, function(extCtx) {
+							var fileName = extCtx.fileName;
+							var filePath = extCtx.filePath;
+							extCtx.project.readTextFile(filePath, function(err, code) {
 								if (err != null) {
 									Main.console.warn(err);
 									next();
 									return;
 								}
-								var gml1 = fn(fileName, filePath, code);
+								var gml1 = fn(fileName, extCtx.fileFull, code);
 								if (gml1 != null && gml1 != code) {
-									pj.writeTextFileSync(filePath, gml1);
+									extCtx.project.writeTextFileSync(filePath, gml1);
 								}
 								next();
 							});
-						}
+						});
 						next();
-					});
+					}));
 				};
 				case "GMRoom": if (opt.checkRooms) {
 					ensureName();
-					var rccName = "roomCreationCodes(" + resName + ")";
-					var rccPath = Path.directory(resPath) + "\\RoomCreationCode.gml";
-					if (pj.existsSync(rccPath)) {
-						filesLeft += 1;
-						pj.readTextFile(rccPath, function(error, code) {
-							if (error == null) {
-								var gml1 = fn(rccName, rccPath, code);
-								if (gml1 != null && gml1 != code) {
-									FileWrap.writeTextFileSync(rccPath, gml1);
-								}
-							} else Main.console.warn(error);
-							next();
-						});
-					}
+					ctx.proc(packResCtx(), function(resCtx) {
+						var rccName = "roomCreationCodes(" + resCtx.resName + ")";
+						var rccPath = Path.directory(resCtx.resPath) + "\\RoomCreationCode.gml";
+						if (resCtx.project.existsSync(rccPath)) {
+							resCtx.project.readTextFile(rccPath, function(error, code) {
+								if (error == null) {
+									var gml1 = fn(rccName, rccPath, code);
+									if (gml1 != null && gml1 != code) {
+										FileWrap.writeTextFileSync(rccPath, gml1);
+									}
+								} else Main.console.warn(error);
+								next();
+							});
+						} else next();
+					});
 				};
 			} // switch (can continue)
 		} // for
-		next();
+		if (ctx.count == 0 && ctx.queue.length == 0) done();
 	}
 }
